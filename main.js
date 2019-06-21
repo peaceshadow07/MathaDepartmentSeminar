cc1ee28304c265cbbf695d7b2f8b534d82a14ad6
Satan7797 <srajbir070@gmail.com>
2019-06-21 21:47:05 +0530

    container

--- main.js
+++ main.js
@@ -9,33 +9,32 @@ var con = document.getElementById('con');
 var display = document.getElementById('display');
 
 ab.onclick = async function () {
-
-    display.innerHTML = await '<object type="text/html" data="aboutconference.html" ></object>';
+    display.innerHTML = await '<object type="text/html" data="aboutconference.html" class="container"></object>';
 }
 
 ac.onclick = async function () {
-    display.innerHTML = await '<object type="text/html" data="aboutcollege.html" ></object>';
+    display.innerHTML = await '<object type="text/html" data="aboutcollege.html" class="container"></object>';
 }
 
 com.onclick = async function() {
-    display.innerHTML = await '<object type="text/html" data="committee.html" ></object>';
+    display.innerHTML = await '<object type="text/html" data="committee.html" class="container"></object>';
 }
 sp.onclick = async function() {
-    display.innerHTML = await '<object type="text/html" data="speakers.html" ></object>';
+    display.innerHTML = await '<object type="text/html" data="speakers.html" class="container"></object>';
 }
 
 pr.onclick = async function() {
-    display.innerHTML = await '<object type="text/html" data="program.html" ></object>';
+    display.innerHTML = await '<object type="text/html" data="program.html" class="container"></object>';
 }
 
 par.onclick = async function() {
-    display.innerHTML = await '<object type="text/html" data="participants.html" ></object>';
+    display.innerHTML = await '<object type="text/html" data="participants.html" class="container"></object>';
 }
 
 reg.onclick = async function() {
-    display.innerHTML = await '<object type="text/html" data="register.html" ></object>';
+    display.innerHTML = await '<object type="text/html" data="register.html" class="container"></object>';
 }
 
 con.onclick = async function() {
-    display.innerHTML = await '<object type="text/html" data="contact.html" ></object>';
+    display.innerHTML = await '<object type="text/html" data="contact.html" class="container"></object>';
 }
